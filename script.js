16c8a9423a4bcb417f18bd36b609095abf38c03e
unforeseenpritam <unforeseenpritam@gmail.com>
2024-08-01 00:05:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function purchaseMove(moveName) {
+    alert(`You have chosen to purchase ${moveName}. The cost is ₹10. Proceeding to payment...`);
+    // Implement payment gateway integration here
+}
