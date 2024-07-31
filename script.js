8bdf9805da351cd1ac492bcc9c18b181b22d34ea
unforeseenpritam <unforeseenpritam@gmail.com>
2024-08-01 01:25:18 +0530

    Update and rename script.js to I

--- script.js
+++
@@ -1,4 +0,0 @@
-function purchaseMove(moveName) {
-    alert(`You have chosen to purchase ${moveName}. The cost is ₹10. Proceeding to payment...`);
-    // Implement payment gateway integration here
-}
